000c126b26c092c836d87e4ccae8b64751e84459
MusaKamau <141785937+MusaKamau@users.noreply.github.com>
2023-08-14 03:09:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+
+    <section id="features" class="section-padding">
+        <div class="container">
+            <div class="section-header text-center">
+                <h2 class="section-title wow fadeInDown">Awesome Features</h2>
+                <div class="shape wow fadeInDown"></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                    <div class="content-left">
+                        <div class="box-item animated wow fadeInLeft">
+                            <span class="icon">
+<i class="fas fa-rocket"></i>
+</span>
+                            <div class="text">
+                                <h4>Display</h4>
+                                <p>The Galaxy Z Fold 5 has a 7.6-inch Dynamic AMOLED 2X display with a 120Hz refresh rate. The outer display is a 6.2-inch Dynamic AMOLED 2X display with a 120Hz refresh rate.</p>
+                            </div>
+                        </div>
+                        <div class="box-item animated wow fadeInLeft" data-wow-delay="0.6s">
+                            <span class="icon">
+<i class="fas fa-tablet-alt"></i>
+</span>
+                            <div class="text">
+                                <h4>Processor</h4>
+                                <p>
+The Galaxy Z Fold 5 is powered by the Qualcomm Snapdragon 8 Gen 2 for Galaxy processor.
+</p>
+                            </div>
+                        </div>
+                        <div class="box-item animated wow fadeInLeft" data-wow-delay="0.9s">
+                            <span class="icon">
+<i class="fas fa-cog"></i>
+</span>
+                            <div class="text">
+                                <h4>RAM</h4>
+                                <p>The Galaxy Z Fold 5 has 12GB of RAM.
+Storage: The Galaxy Z Fold 5 has 256GB, 512GB, or 1TB of internal storage.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                    <div class="show-box animated wow fadeInUp">
+                        <iframe src="https://drive.google.com/file/d/1oUacjZmKSvjqNoPQNtEdpU7wfSNmbpYP/preview" width="640" height="480" allow="autoplay"></iframe>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                    <div class="content-right">
+                        <div class="box-item animated wow fadeInRight">
+                            <span class="icon">
+<i class="fas fa-pen-nib"></i>
+</span>
+                            <div class="text">
+                                <h4>Camera</h4>
+                                <p>The Galaxy Z Fold 5 has a triple-lens rear camera system with a 50-megapixel main sensor, a 12-megapixel ultrawide sensor, and a 10-megapixel telephoto sensor. It also has a 10-megapixel front-facing camera.</p>
+                            </div>
+                        </div>
+                        <div class="box-item animated wow fadeInRight" data-wow-delay="0.6s">
+                            <span class="icon">
+<i class="fas fa-layer-group"></i>
+</span>
+                            <div class="text">
+                                <h4>Battery</h4>
+                                <p>The Galaxy Z Fold 5 has a 4,400mAh battery with support for 25W wired charging and 15W wireless charging.</p>
+                            </div>
+                        </div>
+                        <div class="box-item animated wow fadeInRight" data-wow-delay="0.9s">
+                            <span class="icon">
+<i class="fas fa-feather-alt"></i>
+</span>
+                            <div class="text">
+                                <h4>Software</h4>
+                                <p>The Galaxy Z Fold 5 runs Android 13.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
